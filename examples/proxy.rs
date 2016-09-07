758e9c70c46da0c781ceafe9774971642180c2d0
Andy Grove <andygrove73@gmail.com>
2016-09-07 16:22:51 -0600

    roughing out new proxy approach

--- examples/proxy.rs
+++ examples/proxy.rs
@@ -67,28 +67,27 @@ fn main() {
     lp.run(server).unwrap();
 }
 
-#[derive(Debug)]
-pub enum Direction {
-    Request, Response
-}
-
-struct MyHandler {
-    direction: Direction
-}
-
 impl PacketHandler for MyHandler {
-    fn handle(&self, p: &Packet) {
-        println!("Handling {:?} packet:", self.direction);
+
+    fn handle_request(&self, p: &Packet) -> Action {
+        println!("Request:");
         print_packet_chars(&p.bytes);
+        Action::Forward
     }
+
+    fn handle_request(&self, p: &Packet) -> Action {
+        println!("Response:");
+        print_packet_chars(&p.bytes);
+        Action::Forward
+    }
+
 }
 
 impl Drop for MyHandler {
     fn drop(&mut self) {
-        println!("Dropping {:?} handler", self.direction);
+        println!("Dropping handler");
     }
 }
-
 
 #[allow(dead_code)]
 fn print_packet_chars(buf: &[u8]) {
--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,13 @@ extern crate futures_cpupool;
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, Read, Write, Error, ErrorKind};
 use std::net::{SocketAddr, Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6};
 use std::net::Shutdown;
 use std::str;
 use std::time::Duration;
 
-use futures::{Future, Poll};
+use futures::{Future, Poll, Async};
 use futures::stream::Stream;
 use futures_cpupool::CpuPool;
 use tokio_core::{Loop, LoopHandle, TcpStream};
@@ -28,8 +28,22 @@ pub struct Packet<'a> {
     pub bytes: &'a [u8]
 }
 
+impl<'a> Packet<'a> {
+
+    fn packet_type(&self) -> u8 {
+        self.bytes[4]
+    }
+}
+
+pub enum Action<'a> {
+    Forward,                // forward the packet unmodified
+    Mutate(Packet<'a>),         // mutate the packet
+    Respond(Vec<Packet<'a>>)    // handle directly and optionally return some packets
+}
+
 pub trait PacketHandler {
-    fn handle(&self, p: &Packet);
+    fn handle_request(&self, p: &Packet) -> Action;
+    fn handle_response(&self, p: &Packet) -> Action;
 }
 
 fn parse_packet_length(header: &[u8]) -> usize {
@@ -46,9 +60,8 @@ pub struct Client {
 impl Client {
 
     pub fn serve<H: PacketHandler + 'static>(self, conn: TcpStream,
-                                             request_handler: H,
-                                             response_handler: H
-                                        ) -> Box<Future<Item = (u64, u64), Error = io::Error>> {
+                                             handler: H,
+                                        ) -> Box<Future<Item = (u64, u64), Error = Error>> {
 
 
         let addr = Ipv4Addr::new(127, 0, 0, 1);
@@ -75,92 +88,153 @@ impl Client {
         Box::new(pair.and_then(move |(c1, c2)| {
             let c1 = Rc::new(c1);
             let c2 = Rc::new(c2);
-
-            let half1 = Transfer::new(c1.clone(), c2.clone(), request_handler);
-            let half2 = Transfer::new(c2, c1, response_handler);
-            half1.join(half2)
+            Pipe::new(c1, c2, handler)
         }))
     }
 }
 
-struct Transfer<H: PacketHandler + 'static> {
-    reader: Rc<TcpStream>,
-    writer: Rc<TcpStream>,
-    handler: H,
-    buf: Vec<u8>,
+struct Conn {
+    stream: Rc<TcpStream>,
+    read_buf: Vec<u8>,
+    write_buf: Vec<u8>,
     pos: usize,
-    amt: u64,
+    read_amt: u64,
+    write_amt: u64,
 }
 
-impl<H> Transfer<H> where H: PacketHandler + 'static {
-    fn new(reader: Rc<TcpStream>,
-           writer: Rc<TcpStream>,
-           handler: H
-           ) -> Transfer<H> {
+impl Conn {
 
-        Transfer {
-            reader: reader,
-            writer: writer,
-            handler: handler,
-            buf: vec![0u8; 4096],
-            pos: 0,
-            amt: 0,
-        }
-    }
-}
-
-impl<H> Future for Transfer<H> where H: PacketHandler + 'static {
-    type Item = u64;
-    type Error = io::Error;
-
-    fn poll(&mut self) -> Poll<u64, io::Error> {
-
+    fn read(&mut self) -> Poll<Option<Packet>, io::Error> {
         loop {
+            try_ready!(self.stream.poll_read());
 
-            //TODO: there are definitely race conditions here that will need to be fixed soon
-            try_ready!(self.reader.poll_read());
-            try_ready!(self.writer.poll_write());
-
-            //TODO: ensure there is capacity in the buffer for further reads
-            let n = try_nb!((&*self.reader).read(&mut self.buf[self.pos..]));
+            let n = try_nb!((&*self.stream).read(&mut self.read_buf[self.pos..]));
             if n == 0 {
-                try!(self.writer.shutdown(Shutdown::Write));
-                return Ok(self.amt.into())
+                return Err(Error::new(ErrorKind::Other, "uh oh"));
             }
-            self.amt += n as u64;
+            self.read_amt += n as u64;
             self.pos += n;
 
-            // do we have a header
-            while self.pos > 3 {
-                let l = parse_packet_length(&self.buf);
+            //TODO: parse packet from buffer
 
-                // do we have the whole packet?
-                let s = 4 + l;
-                if self.pos >= s {
-
-                    // invoke the packet handler
-                    {
-                        let packet = Packet { bytes: &self.buf[0..s] };
-                        self.handler.handle(&packet);
-                    }
-
-                    // write the packet
-                    let m = try!((&*self.writer).write(&self.buf[0..s]));
-                    assert_eq!(s, m);
-
-                    // remove this packet from the buffer
-                    //TODO: must be more efficient way to do this
-                    for _ in 0..s {
-                        self.buf.remove(0);
-                    }
-
-                    self.pos -= s;
-                } else {
-                    println!("not enough bytes for packet");
-                    break;
-                }
-            }
+            return Ok(Async::Ready(None));
         }
     }
+
+    fn write(&self) {
+        //try_ready!(self.stream.poll_write());
+        panic!("");
+    }
+}
+
+struct Pipe<H: PacketHandler + 'static> {
+    client: Conn,
+    server: Conn,
+    handler: H,
+}
+
+impl<H> Pipe<H> where H: PacketHandler + 'static {
+    fn new(client: Rc<TcpStream>,
+           server: Rc<TcpStream>,
+           handler: H
+           ) -> Pipe<H> {
+
+        Pipe {
+            client: Conn { stream: client, read_buf: vec![0u8; 4096], write_buf: vec![0u8; 4096], pos: 0, read_amt: 0, write_amt: 0 } ,
+            server: Conn { stream: server, read_buf: vec![0u8; 4096], write_buf: vec![0u8; 4096], pos: 0, read_amt: 0, write_amt: 0 },
+            handler: handler,
+        }
+    }
+}
+
+impl<H> Future for Pipe<H> where H: PacketHandler + 'static {
+    type Item = (u64,u64);
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<(u64,u64), io::Error> {
+        loop {
+
+            // try reading from client
+            let client_read = match self.client.read() {
+                Ok(Async::Ready(None)) => {},
+                Ok(Async::Ready(Some(p))) => {
+                    match self.handler.handle_request(&p) {
+                        Action::Forward => {},
+                        Action::Mutate(p2) => {},
+                        Action::Respond(v) => {},
+                    }
+                },
+                Ok(Async::NotReady) => {},
+                Err(e) => {}
+            };
+
+            // try writing to server
+//            let server_write = match self.server.write() {
+//                Ok(None) => {},
+//                Ok(Some(p)) => {
+//                    match self.handler.handle_request(&p) {
+//                        Action::Forward => {},
+//                        Action::Mutate(p2) => {},
+//                        Action::Respond(v) => {},
+//                    }
+//                },
+//                Err(e) => {}
+//            };
+
+        }
+    }
+
+
+            // just so the code compiles during the refactor
+     //       return Ok((123_u64, 456_u64).into())
+
+
+            //        loop {
+//
+//            //TODO: there are definitely race conditions here that will need to be fixed soon
+//            try_ready!(self.client.poll_read());
+//            try_ready!(self.server.poll_write());
+//
+//            //TODO: ensure there is capacity in the buffer for further reads
+//            let n = try_nb!((&*self.reader).read(&mut self.buf[self.pos..]));
+//            if n == 0 {
+//                try!(self.writer.shutdown(Shutdown::Write));
+//                return Ok(self.amt.into())
+//            }
+//            self.amt += n as u64;
+//            self.pos += n;
+//
+//            // do we have a header
+//            while self.pos > 3 {
+//                let l = parse_packet_length(&self.buf);
+//
+//                // do we have the whole packet?
+//                let s = 4 + l;
+//                if self.pos >= s {
+//
+//                    // invoke the packet handler
+//                    {
+//                        let packet = Packet { bytes: &self.buf[0..s] };
+//                        self.handler.handle(&packet);
+//                    }
+//
+//                    // write the packet
+//                    let m = try!((&*self.writer).write(&self.buf[0..s]));
+//                    assert_eq!(s, m);
+//
+//                    // remove this packet from the buffer
+//                    //TODO: must be more efficient way to do this
+//                    for _ in 0..s {
+//                        self.buf.remove(0);
+//                    }
+//
+//                    self.pos -= s;
+//                } else {
+//                    println!("not enough bytes for packet");
+//                    break;
+//                }
+//            }
+//        }
+//    }
 }
 
